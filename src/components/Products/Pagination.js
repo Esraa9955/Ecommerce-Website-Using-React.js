e4ce6f354c8f5f473dc8648c708e70d81f98e0c9
Aisha Soliman <aeshasoliman12@gmail.com>
2024-01-21 23:12:32 +0200

    styling

--- src/components/Products/Pagination.js
+++ src/components/Products/Pagination.js
@@ -2,23 +2,24 @@ import ProductList from "./ProductList";
 import { axiosInstance } from '../../apis/config';
 import { useEffect, useState } from "react";
 import ReactPaginate from 'react-paginate';
+// import "./Pagination.css"
 
 export default function Pagination(props) {
-    
-  const [products,setProduct]=useState([]);
-  var itemsPerPage = 4;
+  const [products, setProduct] = useState([]);
+  const itemsPerPage = 4;
   const [itemOffset, setItemOffset] = useState(0);
   const endOffset = itemOffset + itemsPerPage;
-  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
   const currentItems = products.slice(itemOffset, endOffset);
   const pageCount = Math.ceil(products.length / itemsPerPage);
-  
-  useEffect(()=>{
-    axiosInstance.get("/products").then((res)=>{console.log('Response',res.data);setProduct((res.data.products));})
-    .catch((err) => console.log('Error',err));
-  },[itemOffset, itemsPerPage]);
 
-  console.log("This should be data", products)
+  useEffect(() => {
+    axiosInstance.get("/products")
+      .then((res) => {
+        console.log('Response', res.data);
+        setProduct(res.data.products);
+      })
+      .catch((err) => console.log('Error', err));
+  }, [itemOffset, itemsPerPage]);
 
   const handlePageClick = (event) => {
     const newOffset = (event.selected * itemsPerPage) % products.length;
@@ -28,27 +29,33 @@ export default function Pagination(props) {
     setItemOffset(newOffset);
   };
 
-  console.log("More data:", products)
-
-return (
+  return (
     <>
-    <ProductList products={currentItems} />
-    <ReactPaginate
-        activeClassName={'item active '}
-        breakClassName={'item break-me '}
-        breakLabel={'...'}
-        containerClassName={'pagination'}
-        disabledClassName={'disabled-page'}
+      <ProductList products={currentItems} />
+
+      <ReactPaginate
+        activeClassName='active'
+        // breakClassName='pagination-break-me'
+        breakLabel='...'
+        containerClassName='pagination justify-content-center'
+        // disabledClassName='pagination-disabled-page'
         marginPagesDisplayed={2}
-        nextClassName={"item next "}
-        nextLabel={<div style={{ fontSize: 18, width: 150 }}>Next</div>}
+        // nextClassName='pagination-next'
+        nextLabel={"Next"}
         onPageChange={handlePageClick}
         pageCount={pageCount}
-        pageClassName={'item pagination-page '}
+        pageClassName={"page-item"}
         pageRangeDisplayed={2}
-        previousClassName={"item previous"}
-        previousLabel={<div style={{ fontSize: 18, width: 150 }}>Previous</div>}
-    />
-  </>
-)
+        // previousClassName='pagination-previous'
+        previousLabel={"Previous"}
+        pageLinkClassName={"page-link"}
+        previousClassName={"page-item"}
+        previousLinkClassName={"page-link"}
+        nextClassName={"page-item"}
+        nextLinkClassName={"page-link"}
+        breakClassName={"page-item"}
+        breakLinkClassName={"page-link"}
+      />
+    </>
+  );
 }
